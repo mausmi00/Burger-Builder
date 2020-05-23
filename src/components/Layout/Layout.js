acdf41f083e95ebe1e3fecb43e82c710ff82f7ac
mausmi00 <mausmi000@gmail.com>
2020-05-23 06:00:24 -0700

    initial commit

---
+++ src/components/Layout/Layout.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import classes from './Layout.module.css';
+import Toolbar from '../Navigation/Toolbar/Toolbar'
+const layout = (props) => (    
+    <>
+            <Toolbar />
+            <main className = {classes.Content}>
+                 {props.children}
+            </main>
+    </>
+);
+
+export default layout;
