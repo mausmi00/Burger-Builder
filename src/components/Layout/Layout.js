f410f472a7f609c5fcc0445898b38e74257eecd4
mausmi00 <mausmi000@gmail.com>
2020-05-23 18:37:28 -0700

    DrawerToggle implemented

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -5,7 +5,7 @@ import SideDrawer from "../Navigation/SideDrawer/SideDrawer";
 
 class Layout extends Component {
     state = {
-        showSideDrawer: true
+        showSideDrawer: false
 
     }
 
@@ -13,11 +13,15 @@ class Layout extends Component {
         this.setState({showSideDrawer: false})
     }
 
+    sideDrawerOpenHandler = () => {
+      this.setState({showSideDrawer: true})
+    }
+
 
   render() {
     return(
       <>
-        <Toolbar />
+        <Toolbar open = {this.sideDrawerOpenHandler}/>
         <SideDrawer open={this.state.showSideDrawer} closed = {this.sideDrawerClosedHandler}/>
         <main className={classes.Content}>{this.props.children}</main>
       </>
--- src/components/UI/Modal/Modal/Modal.js
+++ src/components/UI/Modal/Modal/Modal.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import classes from './Modal.module.css'
-import Backdrop from '../Backdrop/Backdrop'
+import Backdrop from '../../Backdrop/Backdrop'
 
 const modal = (props) => (
     <>
