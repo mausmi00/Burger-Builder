cffa638de48eb8134dcae9c1fc1fefa988dc8b10
mausmi00 <mausmi000@gmail.com>
2020-05-24 06:16:10 -0700

    sending order request to database

--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -3,6 +3,7 @@ import Burger from "../../components/Burger/Burger";
 import BuildControls from "../../components/Burger/BuildControls/BuildControls";
 import Modal from "../../components/UI/Modal/Modal/Modal";
 import OrderSummary from "../../components/Burger/OrderSummary/OrderSummary";
+import axios from "../../axios-orders";
 
 const INGREDIENT_PRICES = {
   salad: 0.5,
@@ -77,7 +78,28 @@ class BurgerBuilder extends Component {
   };
 
   purchaseContinueHandler = () => {
-    alert("You continue!!");
+    //alert("You continue!!");
+    const order = {
+      ingredients: this.state.ingredients,
+      //should calculate the price in server so that user cannot manipulate it
+      price: this.state.totalPrice,
+      customer: {
+        name: "Mausmi Arya",
+        address: {
+          street: "Teststreet 1",
+          zipCode: "121231",
+          country: "Canada",
+        },
+        email: "test@test.com",
+      },
+      deliveryMethod: "fastest",
+    };
+    axios.post("/orders.json", order)
+    .then(response => {
+      console.log(response)
+    }).catch(
+      error => console.log(error)
+    )
   };
 
   render() {
@@ -113,5 +135,4 @@ class BurgerBuilder extends Component {
     );
   }
 }
-
 export default BurgerBuilder;
