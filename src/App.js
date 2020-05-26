af64a8e979730e2e422134f2937db5cfd968da4f
mausmi00 <mausmi000@gmail.com>
2020-05-26 07:16:15 -0700

    implemented orders page

--- src/App.js
+++ src/App.js
@@ -3,6 +3,7 @@ import Layout from "./containers/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder.js";
 import CheckOut from "./containers/CheckOut/CheckOut";
 import { Route } from "react-router-dom";
+import Orders from './components/Orders/Orders'
 
 class App extends Component {
   render() {
@@ -11,6 +12,7 @@ class App extends Component {
         <Layout>
           <Route path="/" exact component={BurgerBuilder} />
           <Route path="/checkout" component={CheckOut} />
+          <Route path = '/orders' component={Orders}/>
         </Layout>
       </div>
     );
--- src/containers/BurgerBuilder/BurgerBuilder.js
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -93,6 +93,7 @@ class BurgerBuilder extends Component {
       queryParams.push(encodeURIComponent(i) + '=' + encodeURIComponent(this.state.ingredients[i]));
 
     }
+    queryParams.push('price='+encodeURIComponent(this.state.totalPrice))
     const queryString  = queryParams.join('&')
 
     this.props.history.push({      
