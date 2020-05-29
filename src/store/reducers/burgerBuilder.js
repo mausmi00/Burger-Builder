eb354bb021ae1e2c996d0b770994666c0e59bd10
mausmi00 <mausmi000@gmail.com>
2020-05-29 07:55:47 -0700

    refactored reducers

--- src/store/reducers/burgerBuilder.js
+++ src/store/reducers/burgerBuilder.js
@@ -1,5 +1,5 @@
 import * as actionTypes from "../actions/actionTypes";
-
+import { updateObject } from "../utility";
 const initialState = {
   ingredients: null,
   totalPrice: 4,
@@ -15,36 +15,39 @@ const INGREDIENT_PRICES = {
 const reducer = (state = initialState, action) => {
   switch (action.type) {
     case actionTypes.ADD_INGREDIENT:
-      return {
-        ...state,
-        ingredients: {
-          ...state.ingredients,
-          [action.ingredientName]: state.ingredients[action.ingredientName] + 1,
-        },
+      const updatedIngredient = {
+        [action.ingredientName]: state.ingredients[action.ingredientName] + 1,
+      };
+      const updateIngredients = updateObject(
+        state.ingredients,
+        updatedIngredient
+      );
+      const updatedState = {
+        ingredients: updateIngredients,
         totalPrice: state.totalPrice + INGREDIENT_PRICES[action.ingredientName],
       };
+      return updateObject(state, updatedState);
+
     case actionTypes.REMOVE_INGREDIENT:
-      return {
-        ...state,
-        ingredients: {
-          ...state.ingredients,
-          [action.ingredientName]: state.ingredients[action.ingredientName] - 1,
-        },
+      const updatedIng = {
+        [action.ingredientName]: state.ingredients[action.ingredientName] - 1,
+      };
+      const updateIngs = updateObject(state.ingredients, updatedIng);
+      const updatedSta = {
+        ingredients: updateIngs,
         totalPrice: state.totalPrice - INGREDIENT_PRICES[action.ingredientName],
       };
+      return updateObject(state, updatedSta);
+
     case actionTypes.SET_INGREDIENTS:
-      return {
-        ...state,
+      return updateObject(state, {
         ingredients: action.ingredients,
         totalPrice: 4,
         error: false,
-      };
-    case actionTypes.FETCH_INGREDIENTS_FAILED:
-      return {
-        ...state,
-        error: true,
-      };
+      });
 
+    case actionTypes.FETCH_INGREDIENTS_FAILED:
+      return updateObject(state, { error: true });
     default:
       return state;
   }
