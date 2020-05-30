00fd1872a4c4c3360c76d0618162ec72b5464a03
mausmi00 <mausmi000@gmail.com>
2020-05-29 20:33:14 -0700

    added a logout page

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import CheckOut from "./containers/CheckOut/CheckOut";
 import { Route } from "react-router-dom";
 import Orders from "./components/Orders/Orders";
 import Auth from "./containers/Auth/Auth";
+import Logout from "./containers/Auth/Logout/Logout";
 class App extends Component {
   render() {
     return (
@@ -14,6 +15,7 @@ class App extends Component {
           <Route path="/checkout" component={CheckOut} />
           <Route path="/orders" component={Orders} />
           <Route path="/auth" component={Auth} />
+          <Route path="/logout" component={Logout} />
         </Layout>
       </div>
     );
--- src/containers/Layout/Layout.js
+++ src/containers/Layout/Layout.js
@@ -2,6 +2,7 @@ import React, { Component } from "react";
 import classes from "./Layout.module.css";
 import Toolbar from "../../components/Navigation/Toolbar/Toolbar";
 import SideDrawer from "../../components/Navigation/SideDrawer/SideDrawer";
+import { connect } from "react-redux";
 
 class Layout extends Component {
   state = {
@@ -19,8 +20,12 @@ class Layout extends Component {
   render() {
     return (
       <>
-        <Toolbar open={this.sideDrawerOpenHandler} />
+        <Toolbar
+          isAuth={this.props.isAuthenticated}
+          open={this.sideDrawerOpenHandler}
+        />
         <SideDrawer
+          isAuth={this.props.isAuthenticated}
           open={this.state.showSideDrawer}
           closed={this.sideDrawerClosedHandler}
         />
@@ -30,4 +35,10 @@ class Layout extends Component {
   }
 }
 
-export default Layout;
+const mapStateToProps = (state) => {
+  return {
+    isAuthenticated: state.auth.token !== null,
+  };
+};
+
+export default connect(mapStateToProps)(Layout);
