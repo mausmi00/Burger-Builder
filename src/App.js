c2b888ec8014572ac87ce4d294d7dfbadac03669
mausmi00 <mausmi000@gmail.com>
2020-05-29 22:36:20 -0700

    added auto login functionality

--- src/App.js
+++ src/App.js
@@ -2,11 +2,17 @@ import React, { Component } from "react";
 import Layout from "./containers/Layout/Layout";
 import BurgerBuilder from "./containers/BurgerBuilder/BurgerBuilder.js";
 import CheckOut from "./containers/CheckOut/CheckOut";
-import { Route } from "react-router-dom";
+import { Route, withRouter } from "react-router-dom";
 import Orders from "./components/Orders/Orders";
 import Auth from "./containers/Auth/Auth";
 import Logout from "./containers/Auth/Logout/Logout";
+import { connect } from "react-redux";
+import * as actions from "./store/actions/index";
+
 class App extends Component {
+  componentDidMount() {
+    this.props.onTryAutoSignup();
+  }
   render() {
     return (
       <div>
@@ -22,4 +28,10 @@ class App extends Component {
   }
 }
 
-export default App;
+const mapDispatchToProps = (dispatch) => {
+  return {
+    onTryAutoSignup: () => dispatch(actions.authCheckState()),
+  };
+};
+
+export default withRouter(connect(null, mapDispatchToProps)(App));
